554c0b6f4b82b7ceac741a33b0f8821f6c64d2fe
khoatang <khoa.tang@exoty.com>
2016-09-15 23:26:35 +0700

    fix bugs

--- app/pods/account/controller.js
+++ app/pods/account/controller.js
@@ -2,6 +2,21 @@ var account = angular.module("lehoiviet");
 
 account.controller("accountController", function($rootScope, $scope, userService) {
   $scope.initData = function() {
-    
+    getProfile();
+  };
+
+  $scope.changTab = function(info){
+    $('.infoList a').removeClass('action');
+    $('.infoList #' + info).addClass('action');
+    $('.infoTab section').addClass('hide');
+    $('.' + info + 'Tab').removeClass('hide')
+  },
+
+  getProfile = function() {
+    userService.getProfile($rootScope.uid, function(response){
+      if (response.status == 200) {
+        $scope.user = response.data.data;
+      }
+    });
   };
 });
