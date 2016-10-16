6b54f0930a319b4efa8234d39e7ebd2ac6d07af2
khoatang <khoa.tang@exoty.com>
2016-10-16 18:49:35 +0700

    fetch data

--- app/pods/festival/controller.js
+++ app/pods/festival/controller.js
@@ -10,12 +10,14 @@ festival.controller("festivalController", function($scope, festivalService, date
     festivalService.getById(festivalId, function(response) {
       if (response.status == 200) {
         $scope.festival = response.data.data;
+        $scope.festival.timeBegin = dateHelper.parse($scope.festival.timeBegin);
+        $scope.festival.timeEnd = dateHelper.parse($scope.festival.timeEnd);
       } else {
 
       }
     });
   };
-  
+
   $scope.watchSlide = function () {
     $('#slideImage').modal('show')
   }
