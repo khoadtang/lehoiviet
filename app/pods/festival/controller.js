1e332c953cb3af83554025d7d9543f9e12f33df3
khoatang <khoa.tang@exoty.com>
2016-10-29 01:31:46 +0700

    upload image with cropping

--- app/pods/festival/controller.js
+++ app/pods/festival/controller.js
@@ -1,11 +1,9 @@
 var festival = angular.module("lehoiviet");
 
-festival.controller("festivalController", function($scope, $rootScope, festivalService, dateHelper, $routeParams, imageService) {
+festival.controller("festivalController", function($scope, $rootScope, festivalService, dateHelper, $routeParams, imageService, commentService) {
   $scope.initData = function() {
     $rootScope.currentPage = "festival";
     getFestivalById();
-
-    // if ($scope.festival.)
   };
 
   getFestivalById = function() {
@@ -15,10 +13,40 @@ festival.controller("festivalController", function($scope, $rootScope, festivalS
         $scope.festival = response.data.data;
         $scope.festival.timeBegin = dateHelper.parse($scope.festival.timeBegin);
         $scope.festival.timeEnd = dateHelper.parse($scope.festival.timeEnd);
+
+        updateLikeElementState();
       } else {
 
       }
     });
+  };
+
+  updateLikeElementState = function() {
+    if ($scope.festival != null || $scope.festival != undefined) {
+      festivalService.checkUserLike($scope.festival._id, function(response) {
+        if (response.data.data.code == 0) {
+          var classOfLikeButton = $('.fa-thumbs-o-down');
+
+          classOfLikeButton.addClass('fa-thumbs-o-up');
+          classOfLikeButton.removeClass('fa-thumbs-o-down');
+        } else {
+          var classOfLikeButton = $('.fa-thumbs-o-up');
+
+          classOfLikeButton.addClass('fa-thumbs-o-down');
+          classOfLikeButton.removeClass('fa-thumbs-o-up');
+        }
+      });
+
+      festivalService.getNumberOfLikes($scope.festival._id, function(response) {
+        $scope.festival.statistic.likes = (typeof response.data.data == "number") ? response.data.data : 0;
+      });
+    }
+  };
+
+  updateRatingElementState = function() {
+    if ($scope.festival != null || $scope.festival != undefined) {
+
+    }
   };
 
   $scope.watchSlide = function () {
@@ -40,16 +68,22 @@ festival.controller("festivalController", function($scope, $rootScope, festivalS
     }
 
     festivalService.like($scope.festival._id, function(response) {
-      if (!response.data.isFalse) {
-        var classOfLikeButton = $('.fa-thumbs-o-up');
-        classOfLikeButton.addClass('fa-thumbs-o-down');
-        classOfLikeButton.remove('fa-thumbs-o-up');
-      } else {
-        var classOfLikeButton = $('.fa-thumbs-o-down');
-        classOfLikeButton.addClass('fa-thumbs-o-up');
-        classOfLikeButton.remove('fa-thumbs-o-down');
-      }
-    })
+      updateLikeElementState();
+    });
+  }
+
+  $scope.onRate = function() {
+    console.log("On Rate");
+    if ($rootScope.token == null) {
+      $('#userLogin').modal('show');
+      return;
+    }
+
+    var data = {};
+    data.point = $scope.point;
+    festivalService.rate($scope.festival._id, data, function(response) {
+      console.log(response);
+    });
   }
 
   $scope.onUploadImage = function() {
@@ -91,9 +125,8 @@ festival.controller("festivalController", function($scope, $rootScope, festivalS
   }
 
   $scope.onSubmitImage = function() {
-    console.log($scope.myCroppedImage)
     var formData = new FormData();
-    formData.append("file", $scope.myCroppedImage);
+    formData.append("file", dataURItoBlob($scope.myCroppedImage));
     imageService.uploadImagePost($scope.festival._id, formData, function(response){
       if(response.status == 200) {
         console.log("Upload Successfully");
@@ -101,3 +134,23 @@ festival.controller("festivalController", function($scope, $rootScope, festivalS
     });
   }
 });
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
--- app/services/comment.js
+++ app/services/comment.js
@@ -3,17 +3,17 @@ var app = angular.module("lehoiviet");
 app.service("commentService", function(net) {
     var commentService = {};
 
-    userService.create = function(comment, eventHandler){
-        net.post('/comment/create/', comment, eventHandler);
+    commentService.create = function(id, comment, eventHandler){
+        net.post('/comment/create/'.concat(id), comment, eventHandler);
     };
 
-    userService.deleteById = function(id, eventHandler) {
-        net.get('/comment/delete/'.concat(id), eventHandler);
+    commentService.deleteById = function(id, eventHandler) {
+        net.post('/comment/delete/'.concat(id), eventHandler);
     };
 
-    userService.update = function(comment, eventHandler){
-        net.post('/comment/update/', comment, eventHandler);
+    commentService.update = function(id, comment, eventHandler){
+        net.post('/comment/update/'.concat(id), comment, eventHandler);
     };
-   
+
     return commentService;
 });
