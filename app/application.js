d40cb0d8f2907bf1573f4023d99a74eaed998fac
nvs2394 <nvs2394@gmail.com>
2016-12-09 01:26:10 +0700

    (Feat) Live stream

--- app/application.js
+++ app/application.js
@@ -1,119 +1,129 @@
 var app = angular.module("lehoiviet", ["ngRoute", "component", "ngCookies", "ngSanitize", "ng.ckeditor", "ngImgCrop", "ngMap"]);
 
-app.config(['$routeProvider', function ($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl : 'app/pods/home/view.html',
-                    controller: "homeController",
-                    controllerAs: 'home'
-                })
-                .when('/festivals', {
-                    templateUrl : 'app/pods/festivals/view.html',
-                    controller: "festivalsController",
-                    controllerAs: 'festivals'
-                })
-                .when('/festival/create', {
-                    templateUrl : 'app/pods/create-festival/view.html',
-                    controller: "createFestivalController",
-                    controllerAs: 'createFestival'
-                })
-                .when('/festival/:festivalId', {
-                    templateUrl : 'app/pods/festival/view.html',
-                    controller: "festivalController",
-                    controllerAs: 'festival'
-                })
-                .when('/festival/update/:festivalId', {
-                    templateUrl : 'app/pods/create-festival/view.html',
-                    controller: "createFestivalController",
-                    controllerAs: 'festival'
-                })
-                .when('/video', {
-                    templateUrl : 'app/pods/video/view.html',
-                    controller: "videoController",
-                    controllerAs: 'video'
-                })
-                .when('/blog', {
-                    templateUrl : 'app/pods/blog/view.html',
-                    controller: "blogController",
-                    controllerAs: 'blog'
-                })
-                .when('/blog/create', {
-                    templateUrl : 'app/pods/create-blog/view.html',
-                    controller: "createBlogController",
-                    controllerAs: 'createBlog'
-                })
-                .when('/user/profile', {
-                    templateUrl : 'app/pods/user/view.html',
-                    controller: "userController",
-                    controllerAs: 'user'
-                })
-                .when('/user/profile/:userId', {
-                    templateUrl : 'app/pods/user/view.html',
-                    controller: "userController",
-                    controllerAs: 'user'
-                })
-                .when('/account/setting', {
-                    templateUrl : 'app/pods/account/view.html',
-                    controller: "accountController",
-                    controllerAs: 'account'
-                })
-                .when('/live', {
-                    templateUrl : 'app/pods/live/view.html',
-                    controller: "liveController",
-                    controllerAs: 'live'
-                })
-                .when('/404', {
-                    templateUrl : 'app/pods/404/view.html',
-                    controller: "errorController",
-                    controllerAs: 'error'
-                })
-                .otherwise({
-                    redirectTo: '/404'
-                });
-        }]);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/pods/home/view.html',
+            controller: "homeController",
+            controllerAs: 'home'
+        })
+        .when('/festivals', {
+            templateUrl: 'app/pods/festivals/view.html',
+            controller: "festivalsController",
+            controllerAs: 'festivals'
+        })
+        .when('/festival/create', {
+            templateUrl: 'app/pods/create-festival/view.html',
+            controller: "createFestivalController",
+            controllerAs: 'createFestival'
+        })
+        .when('/festival/:festivalId', {
+            templateUrl: 'app/pods/festival/view.html',
+            controller: "festivalController",
+            controllerAs: 'festival'
+        })
+        .when('/festival/update/:festivalId', {
+            templateUrl: 'app/pods/create-festival/view.html',
+            controller: "createFestivalController",
+            controllerAs: 'festival'
+        })
+        .when('/video', {
+            templateUrl: 'app/pods/video/view.html',
+            controller: "videoController",
+            controllerAs: 'video'
+        })
+        .when('/blog', {
+            templateUrl: 'app/pods/blog/view.html',
+            controller: "blogController",
+            controllerAs: 'blog'
+        })
+        .when('/blog/create', {
+            templateUrl: 'app/pods/create-blog/view.html',
+            controller: "createBlogController",
+            controllerAs: 'createBlog'
+        })
+        .when('/user/profile', {
+            templateUrl: 'app/pods/user/view.html',
+            controller: "userController",
+            controllerAs: 'user'
+        })
+        .when('/user/profile/:userId', {
+            templateUrl: 'app/pods/user/view.html',
+            controller: "userController",
+            controllerAs: 'user'
+        })
+        .when('/account/setting', {
+            templateUrl: 'app/pods/account/view.html',
+            controller: "accountController",
+            controllerAs: 'account'
+        })
+        .when('/lives', {
+            templateUrl: 'app/pods/lives/list.html',
+            controller: "livesController",
+            controllerAs: 'lives'
+        })
+        .when('/live/create', {
+            templateUrl: 'app/pods/create-live/live.html',
+            controller: "createLiveController",
+            controllerAs: 'createLive'
+        })
+        .when('/live/:streamId', {
+            templateUrl: 'app/pods/create-live/view.html',
+            controller: "createLiveController",
+            controllerAs: 'createLive'
+        })
+        .when('/404', {
+            templateUrl: 'app/pods/404/view.html',
+            controller: "errorController",
+            controllerAs: 'error'
+        })
+        .otherwise({
+            redirectTo: '/404'
+        });
+}]);
 
 app.constant("ENV", {
-  apiUrl: "http://125.253.113.15:3000",
-  gateWay: "http://125.253.113.15:3000"
-    /*apiUrl: "http://api.lehoiviet.vn",
-    gateWay: "http://api.lehoiviet.vn"*/
+    apiUrl: "http://127.0.0.1:3000",
+    gateWay: "http://125.253.113.15:3000"
+        /*apiUrl: "http://api.lehoiviet.vn",
+        gateWay: "http://api.lehoiviet.vn"*/
 });
 
 app.constant("FestivalStatus", {
-  "1": "Bản Nháp",
-  "2": "Chờ Duyệt",
-  "3": "Đã Duyệt"
+    "1": "Bản Nháp",
+    "2": "Chờ Duyệt",
+    "3": "Đã Duyệt"
 });
 
 app.controller("appController", function($scope, $rootScope, userService, gatewayService, festivalService, $route, FestivalStatus) {
-  $scope.init = function() {
+    $scope.init = function() {
 
-    $rootScope.status = FestivalStatus;
-    $rootScope.token = null;
+        $rootScope.status = FestivalStatus;
+        $rootScope.token = null;
 
-    userService.autoLogin(function(response){
-        var data = response.data;
-        $rootScope.token = data.token;
-        $rootScope.email = data.user.email;
-        $rootScope.avatar = data.user.avatar;
-        $rootScope.firstName = data.user.firstName;
-        $rootScope.uid = data.user._id;
+        userService.autoLogin(function(response) {
+            var data = response.data;
+            $rootScope.token = data.token;
+            $rootScope.email = data.user.email;
+            $rootScope.avatar = data.user.avatar;
+            $rootScope.firstName = data.user.firstName;
+            $rootScope.uid = data.user._id;
 
-        festivalService.getNotifiedFestival(function(response){
-          $rootScope.notification = response.data.data;
-          angular.forEach($rootScope.notification, function(value, key){
-            if(!value.data.notifyStatus) {
-              if ($rootScope.notification.unseen == null) {
-                $rootScope.notification.unseen = 0;
-              }
-              $rootScope.notification.unseen++;
-            }
-          });
+            festivalService.getNotifiedFestival(function(response) {
+                $rootScope.notification = response.data.data;
+                angular.forEach($rootScope.notification, function(value, key) {
+                    if (!value.data.notifyStatus) {
+                        if ($rootScope.notification.unseen == null) {
+                            $rootScope.notification.unseen = 0;
+                        }
+                        $rootScope.notification.unseen++;
+                    }
+                });
+            });
+
+            gatewayService.online();
+            gatewayService.listen();
+
         });
-
-        gatewayService.online();
-        gatewayService.listen();
-
-    });
-  };
+    };
 });
--- app/services/live.js
+++ app/services/live.js
@@ -4,19 +4,24 @@ app.service("liveService", function(net) {
     var liveService = {};
 
     liveService.create = function(data, eventHandler){
-        net.post('/live/create/', data, eventHandler);
+        console.log(data)
+        net.post('/live/create', data, eventHandler);
     };
 
     liveService.update = function(data, eventHandler){
-        net.post('/live/update/', data, eventHandler);
+        net.post('/live/update', data, eventHandler);
     };
 
     liveService.get = function(eventHandler) {
-        net.get('/live/lists/', eventHandler);
+        net.get('/live/list', eventHandler);
     };
 
     liveService.getByAuthor = function(id, eventHandler) {
         net.get('/live/user/'.concat(id), eventHandler);
+    };
+
+    liveService.getByStreamId = function(id, eventHandler) {
+        net.get('/live/show/'.concat(id), eventHandler);
     };
 
     liveService.getByPost = function(id, eventHandler) {
