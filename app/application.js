45b75f519ef861823681cde4f003805dc31fa4cc
HoangHuong <hoangtthuong94@gmail.com>
2016-12-10 00:05:23 +0700

    Add create live stream page

--- app/application.js
+++ app/application.js
@@ -72,6 +72,11 @@ app.config(['$routeProvider', function($routeProvider) {
             controller: "createLiveController",
             controllerAs: 'createLive'
         })
+        .when('/live', {
+            templateUrl: 'app/pods/live/view.html',
+            controller: "liveController",
+            controllerAs: 'live'
+        })
         .when('/404', {
             templateUrl: 'app/pods/404/view.html',
             controller: "errorController",
@@ -83,10 +88,11 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.constant("ENV", {
-    apiUrl: "http://127.0.0.1:3000",
-    gateWay: "http://127.0.0.1:3000"
-        /*apiUrl: "http://api.lehoiviet.vn",
-        gateWay: "http://api.lehoiviet.vn"*/
+    apiUrl: "http://125.253.113.15:3000",
+    gateWay: "http://125.253.113.15:3000"
+
+    /*apiUrl: "http://api.lehoiviet.vn",
+    gateWay: "http://api.lehoiviet.vn"*/
 });
 
 app.constant("FestivalStatus", {
---
+++ app/pods/live/controller.js
@@ -0,0 +1,7 @@
+var live = angular.module("lehoiviet");
+
+live.controller("liveController", function($scope) {
+    $scope.init = function(){
+
+    };
+});
