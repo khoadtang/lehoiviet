5b960b35c7b83df369392a1f02d9e859c4d34485
khoatang <khoa.tang@exoty.com>
2017-01-02 21:16:28 +0700

    fix bugs

--- app/application.js
+++ app/application.js
@@ -103,10 +103,10 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.constant("ENV", {
-    // apiUrl: "https://api.lehoiviet.vn",
-    // gateWay: "https://api.lehoiviet.vn"
-    apiUrl: "http://localhost:3000",
-    gateWay: "http://localhost:3000"
+    apiUrl: "https://api.lehoiviet.vn",
+    gateWay: "https://api.lehoiviet.vn"
+    // apiUrl: "http://localhost:3000",
+    // gateWay: "http://localhost:3000"
     /*apiUrl: "http://api.lehoiviet.vn",
     gateWay: "http://api.lehoiviet.vn"*/
 });
