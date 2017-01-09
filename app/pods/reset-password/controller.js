cd510f54ed0f982c7a66d8b1d7f784199a45135b
khoatang <khoa.tang@exoty.com>
2017-01-09 22:25:54 +0700

    add reset password

--- app/pods/reset-password/controller.js
+++ app/pods/reset-password/controller.js
@@ -1,17 +1,37 @@
 var resetPassword = angular.module("lehoiviet");
 
-resetPassword.controller("resetPasswordController", function($scope, userService, $routeParams) {
+resetPassword.controller("resetPasswordController", function($scope, userService, $routeParams, $timeout) {
   $scope.init = function(){
-
+    $scope.isLoading = false;
   };
 
-  $scope.resetPassword = function(){
+  $scope.rsPassword = function(){
+    if ($scope.password == null || $scope.password == 'undefined'){
+        $scope.alert = "Xin Vui Lòng Nhập Mật Khẩu";
+        return;
+    }
+
+    if ($scope.password != $scope.repassword) {
+      $scope.alert = "Mật Khẩu Phải Giống Nhau";
+      $scope.password = null;
+      $scope.repassword = null;
+
+      return;
+    }
     var data = {};
     data.token = $routeParams.token;
-    data.newPassword = $scope.newPassword;
+    data.newPassword = $scope.password;
+    data.host = 'www';
 
+    console.log(data);
+    $scope.isLoading = true;
     userService.resetPassword(data, function(response){
-      console.log(response);
+      $scope.isLoading = false;
+      $scope.alert = "Đổi Mật Khẩu Thành Công";
+
+      $timeout(function() {
+        window.location = "#/";
+      }, 1000);
     });
   }
 });
